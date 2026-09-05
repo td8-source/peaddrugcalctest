18a5ca6e66c21041da1373f5e2fafc28fa109faf
td8-source <tailadale@gmail.com>
2026-09-05 22:30:09 +1200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Caches the app shell so it opens with no signal.
+const C = "paeds-doses-v1";
+const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request, { ignoreSearch: true }).then(r => r || fetch(e.request).then(res => {
+    const copy = res.clone(); caches.open(C).then(c => c.put(e.request, copy)); return res;
+  }).catch(() => caches.match("./index.html"))));
+});
