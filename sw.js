d614663a7db08f2bdf1ffcc724f44082089c6b42
td8-source <tailadale@gmail.com>
2026-09-05 23:21:27 +1200

    Update cache version from v1 to v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Caches the app shell so it opens with no signal.
-const C = "paeds-doses-v1";
+const C = "paeds-doses-v3";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
